35fe59e74a67403322a719c785317fdb8f6b35bd
tyler <thailer2000@gmail.com>
2024-06-04 18:25:23 -0400

    refactor old functions play game, get human choice

--- code.js
+++ code.js
@@ -1,13 +1,17 @@
+let buttons = document.querySelectorAll(".btn");
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', ()=>{
+        alert(button.textContent);
+    });
+
+});
+
 
 function getComputerChoice(min,max){
     let computerPlay = Math.floor(Math.random() * (max - min + 1) + min);
     return choice(computerPlay)
-};
-
-function getHumanChoice(){
-    let humanChoice = prompt("Elige un numero siendo 1 Papel 2 piedra y 3 tijeras");
-
-    return (choice(humanChoice))
 };
 
 function choice(choice){
@@ -17,44 +21,3 @@ function choice(choice){
 
     return choice;
 }
-
-
-function playGame(){
-    let computerScore = 0;
-    let humanScore = 0; 
-    let round = 1;
-
-    for (let i = round; i <= 5; i++) {
-        round = i
-        console.log("Ronda numero " + round)
-        console.log("El marcador es " + humanScore + " - " + computerScore) 
-        console.log("")
-        playRound()
-    }
-  
-    if(humanScore > computerScore){
-        console.log("El ganador eres tu")
-    }else console.log("Gana la maquina!")
-
-    function playRound(){
-         let humanChoice = getHumanChoice();
-         let computerChoice = getComputerChoice(1,3);
-        
-        if (humanChoice === computerChoice){
-            console.log("Es un empate! " + humanChoice + " " + computerChoice);
-            playRound();
-        }else if(humanChoice == "Piedra" && computerChoice == "Tijera" || humanChoice == "Tijera" && computerChoice == "Papel" || humanChoice == "Papel" && computerChoice == "Piedra") {
-            console.log("Tu ganas! " + humanChoice + " le gana a "+computerChoice );
-            humanScore++
-        }else{
-             console.log("Tu pierdes! " + computerChoice + " le gana a "+humanChoice);
-             computerScore++
-        }
-    }
-
-        
-
-}
-
-
-playGame()
